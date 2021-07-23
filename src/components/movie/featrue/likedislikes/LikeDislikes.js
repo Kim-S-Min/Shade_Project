05f60d99cc6b8f2a6fdff55e899195f7eaff37fc
김상민 <wrt123123123@gmail.com>
2021-07-24 02:55:36 +0900

    model like,dislike

--- src/components/movie/featrue/likedislikes/LikeDislikes.js
+++ src/components/movie/featrue/likedislikes/LikeDislikes.js
@@ -4,16 +4,6 @@ import { IconButton } from "@material-ui/core";
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 
-// 1. 로그인이 되어있는 유저만 좋아요 싫어요를 클릭할 수 있어야 한다.
-// 2. response를 받을 때 해당 (user_id, contents_id, check_like)의 값을 받아야 한다
-// 3. 좋아요, 싫어요를 눌렀을때 check_like의 값이 (null, like, unlike)로 변동이 되어야 한다
-// 4. 좋아요를 눌렀을때 check_like의 값이 null -> like로 변해야 한다
-// 5. 좋아요를 한번 더 눌렀을 때 check_like의 값이 like -> null 로 변해야 한다
-// 6. 싫어요를 눌렀을때 check_like의 값이 null -> unlike 로 변해야 한다
-// 7. 싫어요를 한번 더 눌렀을 때 check_like의 값이 unlike -> null로 변해야 한다.
-// 8. check_like의 값이 like일때 싫어요를 누르면 like -> unlike 로 변해야 한다.
-// 9. check_like의 값이 unlike일때 좋아요를 누르면 unlike -> like로 변해야 한다.
-
 export default function LikeDislikes(props) {
   const [Likes, setLikes] = useState(0)
   const [Dislikes, setDislikes] = useState(0)
@@ -42,6 +32,7 @@ export default function LikeDislikes(props) {
         alert('Like에 대한 정보를 가져오지 못했습니다.');
       }
     });
+
     Axios.post('/api/like/getDislikes', variable).then((response) => {
       if (response.data.success) {
         //얼마나 많은 싫어요를 받았는지
@@ -63,23 +54,23 @@ export default function LikeDislikes(props) {
     if (LikeAction === '') {
       Axios.post('/api/like/upLike', variable).then((response) => {
         if (response.data.success) {
-          setLikes(Likes === "like");
+          setLikes(Likes + 1);
           setLikeAction('liked');
-          console.log("like");
+          console.log("Like");
 
           if (DisLikeAction !== '') {
             setDisLikeAction('');
-            setDislikes(Dislikes === "unlike");
-            console.log("unlike");
+            setDislikes(Dislikes - 1);
+            console.log("Dislike");
           }
         } else {
           alert('Like를 올리지 못했습니다.');
         }
       });
     } else {
-      Axios.post('/api/like/unLike', variable).then((response) => {
+      Axios.post('/api/like/downLike', variable).then((response) => {
         if (response.data.success) {
-          setLikes(Likes === "null");
+          setLikes(Likes === '');
           setLikeAction('');
         } else {
           alert('Like를 내리지 못했습니다.');
@@ -90,7 +81,7 @@ export default function LikeDislikes(props) {
 
   const onDislike = () => {
     if (DisLikeAction !== '') {
-      Axios.post('/api/like/unDislike', variable).then((response) => {
+      Axios.post('/api/like/downDislike', variable).then((response) => {
         if (response.data.success) {
           setDislikes(Dislikes - 1);
           setDisLikeAction('');
